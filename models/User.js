2f3bddbee6247a13fbaf7cc094efbee78ffcc613
Ian Holmes <concord511@gmail.com>
2021-11-14 12:46:55 -0600

    added password hashing and login functionality

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
 
 // definte table columns and configuration
 User.init(
@@ -42,9 +48,20 @@ User.init(
                 // this means the password must be at least four characters long
                 len: [4]
             }
-        }
+        },
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
